ed60a7618a9ceef76811ffd02b42abf0793d38b9
yatish-kbcd <yatishsharma@kbcdindia.com>
2025-11-29 14:35:03 +0530

    added cors

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const cors = require('cors');
 const PORT = process.env.PORT || 5000;
+
+app.use(cors());
 
 app.use(express.json());
 
