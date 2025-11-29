fd9461fd0f890bbe94267259a77a55956f5f6a3f
yatish-kbcd <yatishsharma@kbcdindia.com>
2025-11-29 13:53:03 +0530

    change base url

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [message, setMessage] = useState('');
 
   useEffect(() => {
-    fetch('/api/hello')
+    fetch('http://167.71.238.99:5000/api/hello')
       .then(response => response.json())
       .then(data => setMessage(data.message))
       .catch(error => console.error('Error fetching data:', error));
